2b11ef7e060e02ae0dc30594aab71d6c3a328bf8
Clément Blaise <clementblaise@me>
2020-09-23 16:40:55 +0200

    update build.sh with correct package name and switch to sha256 checksum

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GOOS=linux go build -o ./bin/rqc main.go
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s" -o ./bin/rqc-darwin main.go
-GOOS=windows GOARCH=386 go build -o ./bin/rqc.exe
-md5sum ./bin/rqc ./bin/rqc.exe ./bin/rqc-darwin > ./bin/md5
+GOOS=linux go build -o ./bin/kotaplan main.go
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s" -o ./bin/kotaplan-darwin main.go
+GOOS=windows GOARCH=386 go build -o ./bin/kotaplan.exe
+shasum -a 256 bin/kotaplan bin/kotaplan-darwin bin/kotaplan.exe > ./bin/sha256
