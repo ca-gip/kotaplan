ff125f17d6ac35a0fcae771990833b1615de1f47
Clément Blaise <clementblaise@me.com>
2021-08-27 10:17:06 +0200

    Update doc

--- cmd/root.go
+++ cmd/root.go
@@ -40,25 +40,25 @@ func init() {
 
 	// kubectl
 	rootCmd.PersistentFlags().StringP("kubeconfig", "", k8s.DefaultKubeconfig(), "Path to a kubeconfig")
-	rootCmd.PersistentFlags().StringP("master", "", "", "Address of the Kubernetes API server. Overrides any value in kubeconfig.")
+	rootCmd.PersistentFlags().StringP("master", "", "", "Address of the Kubernetes API server. Overrides any value in kubeconfig")
 
 	// Default claim
-	rootCmd.PersistentFlags().Int64P("default-claim-memory", "", 0, "Amount of Memory for the default claim in GiB. Default is 0.")
-	rootCmd.PersistentFlags().Int64P("default-claim-cpu", "", 0, "Amount of CPU for the default claim in Milli. ex 1000 = 1CPU. Default is 0.")
+	rootCmd.PersistentFlags().Int64P("default-claim-memory", "", 0, "Amount of Memory for the default claim in GiB. (default 0)")
+	rootCmd.PersistentFlags().Int64P("default-claim-cpu", "", 0, "Amount of CPU for the default claim in Milli. ex 1000 = 1CPU. (default 0)")
 
 	// Max per Namespace
-	rootCmd.PersistentFlags().Float64P("ratio-namespace-memory", "", 1, "Ratio of the maximum amount of Memory that can be claim by a namespace. Default is 1 : 100% of the cluster is claimable by a Namespace")
-	rootCmd.PersistentFlags().Float64P("ratio-namespace-cpu", "", 1, "Ratio of the maximum amount of CPU that can be claim by a namespace. Default is 1 : 100% of the cluster is claimable by a Namespace")
+	rootCmd.PersistentFlags().Float64P("ratio-namespace-memory", "", 1, "Ratio of the maximum amount of Memory that can be claim by a namespace. Ex: 0.5 meaning 50% of the cluster is claimable by a Namespace")
+	rootCmd.PersistentFlags().Float64P("ratio-namespace-cpu", "", 1, "Ratio of the maximum amount of CPU that can be claim by a namespace. Ex: 0.5 meaning 50% of the cluster is claimable by a Namespace")
 
 	// Over commit
-	rootCmd.PersistentFlags().Float64P("over-commit-memory", "", 1, "Ratio of the Memory over or under commit. Default is 1 meaning 100 %")
-	rootCmd.PersistentFlags().Float64P("over-commit-cpu", "", 1, "Ratio of the CPU over or under commit. Default is 1 meaning 100 %")
+	rootCmd.PersistentFlags().Float64P("over-commit-memory", "", 1, "Ratio of the Memory over or under commit")
+	rootCmd.PersistentFlags().Float64P("over-commit-cpu", "", 1, "Ratio of the CPU over or under commit")
 
 	// Margin
-	rootCmd.PersistentFlags().Float64P("margin", "", 1.2, "Margin for the recommended spec. Default is 1.2")
+	rootCmd.PersistentFlags().Float64P("margin", "", 1.2, "Margin for the recommended spec")
 
 	// Label
-	rootCmd.PersistentFlags().StringP("labels", "l", "quota=managed", "Match namespace containing a label. Default is quota=managed")
+	rootCmd.PersistentFlags().StringP("labels", "l", "quota=managed", "Match namespace containing a label")
 }
 
 // initConfig reads in config file and ENV variables if set.
